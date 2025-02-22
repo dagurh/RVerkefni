983bdee647511a2ae525ed027910b3202e9ac411
au602716 <au602716@uni.au.dk>
2025-02-22 17:32:49 +0100

    hluti2 progress

--- fyrra_r/pisa.R
+++ fyrra_r/pisa.R
@@ -1,10 +1,72 @@
 install.packages("dplyr")
 library(dplyr)
+library(ggplot2)
 
+#Hluti 1
+
+#a
 d <- read.csv("Gögn/pisa.csv", stringsAsFactors = FALSE)
 
+#b
 d <- d %>% filter(year >= 2003)
 
+#c
 d <- d %>% mutate(mean_grade = rowMeans(select(., math, read, science), na.rm = TRUE))
 
+#d
 table(d$country)
+
+#e
+d <- d %>% filter(country %in% c("ISL", "DNK", "NOR"))
+
+#f
+str(d)
+
+d$year <- as.factor(d$year)
+d$country <- as.factor(d$country)
+
+str(d)
+
+#g
+d$country <- recode(d$country, "ISL" = "Ísland", "DNK" = "Danmörk", "NOR" = "Noregur")
+
+#Hluti 2
+
+#h
+island <- d %>% filter(d$country == "Ísland")
+
+ggplot(island, aes(x = math)) +
+  geom_histogram(binwidth = 10, fill = "lightblue", color = "black") +
+  labs(title = "Einkunnir í stærðfræði meðal íslenskra nemenda",
+       x = "Stærðfræðieinkunn",
+       y = "Fjöldi nemenda") +
+  theme_minimal()
+#miðlæg dreifing, stór hluti nemenda nálægt miðgildi sem er í kringum 520
+
+#i
+ggplot(d, aes(x = country, y = math, fill = country)) +
+  geom_boxplot() +
+  labs(title = "Stærðfræðieinkunnir eftir löndum",
+       x = "Lönd",
+       y = "Stærðfræðieinkunn") +
+  theme_minimal()
+#Ísland er með hæsta miðgildið, danmörk með bestu útlægur og þrengri 25-75%
+
+#j
+ggplot(d, aes(x = country, y = math, fill = country)) +
+  geom_boxplot() +
+  facet_wrap(~ year) +
+  labs(title = "Stærðfræðieinkunnir eftir löndum og árum",
+       x = "Lönd",
+       y = "Stærðfræðieinkunn") +
+  theme_minimal()
+#Noregur stendur sig betur og betur gagnvart hinum löndunum með tímanum
+#En yfir allt fer öllum löndum versnandi (mesti niðurdúrinn frá 2018 til 2022)
+
+ggplot(island, aes(x = math, y = science)) +
+  geom_point(color = "blue", size = 2, alpha = 0.7) +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  labs(title = "Samband milli stærðfræðieinkunna og vísindaeinkunna",
+       x = "Stærðfræðieinkunn",
+       y = "Vísindaeinkunn") +
+  theme_minimal()
